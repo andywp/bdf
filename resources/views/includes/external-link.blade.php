2ded38c1069052c872bc944f21528dbb61962d28
andywp <andy.wijang@gmail.com>
2022-08-30 11:02:53 +0700

    external link

--- resources/views/includes/external-link.blade.php
+++ resources/views/includes/external-link.blade.php
@@ -3,7 +3,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h2 class="title-section text-center">External Link {{ $data }}</h2>
+        <h2 class="title-section text-center">External Link</h2>
       </div>
     </div>
     <div class="row">
